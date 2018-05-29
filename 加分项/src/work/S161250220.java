5cf29ede8026f5ea30320648088c2d0d71212f66
iznauy <zy05160516@126.com>
2018-05-29 23:48:04 +0800

    optimize the implementation of the task managerment

--- 加分项/src/work/S161250220.java
+++ 加分项/src/work/S161250220.java
@@ -42,27 +42,29 @@ import java.io.IOException;
  */
 public class S161250220 extends Schedule {
 
-    private static final int kernel = 2;
+    private static final int kernel = 5;
 
-    private static final int latestTaskBeginner = 0;
-    private static final int cpuStateBeginner = latestTaskBeginner + 4; // CPU个，每个4字节
-    private static final int resourceBeginner = cpuStateBeginner + 4 * kernel;
+    private static final int latestTaskBeginner = 0; // 其实是一个short
+    private static final int lastTaskAmount = latestTaskBeginner + 2;
+    private static final int cpuStateBeginner = lastTaskAmount + 2; // CPU个，每个2字节
+    private static final int resourceBeginner = cpuStateBeginner + 2 * kernel;
     private static final int pcbTableBeginner = resourceBeginner + Constant.MAX_RESOURCE;
-    private static final int pcbBeginner = pcbTableBeginner + 1000 * 4; // 1000个任务，每个任务4
+    private static final int pcbFinishBeginner = pcbTableBeginner + 1000 * 2; // 1000个任务，每个任务2
+    private static final int pcbBeginner = pcbFinishBeginner + 1000; // 之前用1000个比特
 
     private static final int PCB_tidBeginner = 0;
-    private static final int PCB_arrivedTimeBeginner = 4;
-    private static final int PCB_cpuTimeBeginner = 8;
-    private static final int PCB_leftTimeBeginner = 12;
-    private static final int PCB_rsLengthBeginner = 16;
-    private static final int PCB_resourceBeginner = 20;
+    private static final int PCB_arrivedTimeBeginner = 2;
+    private static final int PCB_cpuTimeBeginner = 4;
+    private static final int PCB_leftTimeBeginner = 6;
+    private static final int PCB_rsLengthBeginner = 8;
+    private static final int PCB_resourceBeginner = 10;
 
     // 评估局面好坏的函数，或许不能称他为启发式函数，但是想不到什么好名字了
     private Double heuristic(int taskID) {
         int lastTime = getTaskLastTime(taskID);
         int waitedTime = getTaskWaitTime(taskID);
         Double leastTolerance = priceFunction(lastTime + waitedTime);
-        Double delayTolerance = priceFunction(lastTime + waitedTime + 1);
+        Double delayTolerance = priceFunction(lastTime + waitedTime + 2);
         if (delayTolerance == Double.MAX_VALUE)
             return Double.MAX_VALUE;
         else
@@ -79,46 +81,46 @@ public class S161250220 extends Schedule {
         }
     }
 
-    private void countDownLeft(int taskID){
+    private boolean countDownLeft(int taskID){
         int index = getTaskBeginIndex(taskID);
-        int leftTime = readInteger(index+PCB_leftTimeBeginner);
-        if(leftTime == 0) return;
+        int leftTime = readShort(index+PCB_leftTimeBeginner);
+        if(leftTime == 0) return true;
         leftTime--;
-        writeInteger(index+PCB_leftTimeBeginner, leftTime);
+        writeShort(index+PCB_leftTimeBeginner, leftTime);
+        if (leftTime == 0) {
+            writeFreeMemory(pcbFinishBeginner + taskID, (byte) 1);
+            return true;
+        }
+        return false;
     }
 
     private boolean isTaskFinish(int taskID){
-        int index = getTaskBeginIndex(taskID);
-        int leftTime = readInteger(index+PCB_leftTimeBeginner);
-        return leftTime == 0;
+        return readFreeMemory(pcbFinishBeginner + taskID) == (byte) 1;
     }
 
-    private void cleanAllResource(){
-        for(int i = 0 ; i < Constant.MAX_RESOURCE ; i++){
-            writeFreeMemory(resourceBeginner+i, (byte) 0);
-        }
-    }
-
-    private boolean useResource(int taskID){
-        int index = getTaskBeginIndex(taskID);
-        int length = readInteger(index+PCB_rsLengthBeginner);
-
+    private boolean canUseResource(int taskId) {
+        int index = getTaskBeginIndex(taskId);
+        int length = readShort(index+PCB_rsLengthBeginner);
         for(int i = 0 ; i < length ; i++){
             byte temp = readFreeMemory(index+PCB_resourceBeginner+i);
             if(readFreeMemory(resourceBeginner+temp-1) != 0) return false;
         }
+        return true;
+    }
+
+    private void useResource(int taskID){
+        int index = getTaskBeginIndex(taskID);
+        int length = readShort(index+PCB_rsLengthBeginner);
 
         for(int i = 0 ; i < length ; i++){
             byte temp = readFreeMemory(index+PCB_resourceBeginner+i);
             writeFreeMemory(resourceBeginner+temp-1, (byte) 1);
         }
-
-        return true;
     }
 
     private void clearResource(int taskId) {
         int index = getTaskBeginIndex(taskId);
-        int length = readInteger(index+PCB_rsLengthBeginner);
+        int length = readShort(index+PCB_rsLengthBeginner);
 
         for(int i = 0 ; i < length ; i++) {
             byte temp = readFreeMemory(index + PCB_resourceBeginner + i);
@@ -129,14 +131,27 @@ public class S161250220 extends Schedule {
 
     @Override
     public void ProcessSchedule(Task[] arrivedTask, int[] cpuOperate) {
+
+        int taskAmount = readShort(lastTaskAmount);
+
         if (arrivedTask != null && arrivedTask.length != 0) {
             for (Task task: arrivedTask) {
                 recordTask(task);
+                taskAmount += 1;
             }
         }
 
-        cleanAllResource();
-        int taskNumber = readInteger(latestTaskBeginner);
+        if (taskAmount == 0)
+            return;
+
+        for (int i = 0; i < cpuOperate.length; i++) {
+            int taskId = readShort(cpuStateBeginner + 2 * i);
+            if (taskId != 0) {
+                clearResource(taskId);
+            }
+        }
+
+        int taskNumber = readShort(latestTaskBeginner);
         int cpuNumber = getCpuNumber() - 1;
         for (int i = 0; i <= cpuNumber; i++) {
             int task = -1;
@@ -151,12 +166,11 @@ public class S161250220 extends Schedule {
                 if (current < price)
                     continue;
 
-                if (!useResource(j)) // 没有足够的资源
+                if (!canUseResource(j)) // 没有足够的资源
                     continue;
 
                 task = j;
                 price = current;
-                clearResource(j); // 释放资源，否则就有可能占着资源不放，导致后来的任务不能公正的和当前任务进行比较
 
             }
 
@@ -165,13 +179,15 @@ public class S161250220 extends Schedule {
             else {
                 cpuOperate[i] = task;
                 useResource(task);
-                countDownLeft(task);
+                if (countDownLeft(task)) {
+                    taskAmount -= 1;
+                }
             }
         }
 
 
-        for (int i = 0; i < kernel; i++) {
-            int preTask = readInteger(cpuStateBeginner + 4 * i);
+        for (int i = 0; i <= cpuNumber; i++) {
+            int preTask = readShort(cpuStateBeginner + 2 * i);
             if (preTask == 0) // 遇到了空任务
                 continue;
             for(int j = 0; j <= cpuNumber; j++) {
@@ -188,114 +204,60 @@ public class S161250220 extends Schedule {
 
         for (int i = 0; i < cpuOperate.length; i++) {
             if (cpuOperate[i] != 0)
-                writeInteger(cpuStateBeginner + i * 4, cpuOperate[i]);
+                writeShort(cpuStateBeginner + i * 2, cpuOperate[i]);
         }
+
+        writeShort(lastTaskAmount, taskAmount);
 
     }
 
     private int getTaskWaitTime(int taskId) {
-        return getTimeTick() - readInteger(getTaskBeginIndex(taskId) + PCB_arrivedTimeBeginner);
+        return getTimeTick() - readShort(getTaskBeginIndex(taskId) + PCB_arrivedTimeBeginner);
     }
 
     private int getTaskLastTime(int taskId) {
-        return readInteger(getTaskBeginIndex(taskId) + PCB_leftTimeBeginner);
+        return readShort(getTaskBeginIndex(taskId) + PCB_leftTimeBeginner);
     }
 
     private int getTaskBeginIndex(int taskID){
-        return readInteger(pcbTableBeginner+taskID*4);
+        return readShort(pcbTableBeginner+taskID*2); // 每个长度是2
     }
 
     private int getTaskResourceLength(int taskID){
-        return readInteger(getTaskBeginIndex(taskID)+PCB_rsLengthBeginner);
+        return readShort(getTaskBeginIndex(taskID)+PCB_rsLengthBeginner);
     }
 
     private int getNewTaskBeginIndex(){
-        int latestTaskID = readInteger(latestTaskBeginner);
+        int latestTaskID = readShort(latestTaskBeginner);
         if(latestTaskID == 0) return pcbBeginner;
         return getTaskBeginIndex(latestTaskID)+getTaskResourceLength(latestTaskID)+PCB_resourceBeginner;
     }
 
     private void recordTask(Task task) {
         int newIndex = getNewTaskBeginIndex();
-        writeInteger(newIndex+PCB_tidBeginner, task.tid);
-        writeInteger(newIndex+PCB_arrivedTimeBeginner, getTimeTick());
-        writeInteger(newIndex+PCB_cpuTimeBeginner, task.cpuTime);
-        writeInteger(newIndex+PCB_leftTimeBeginner, task.cpuTime);
-        writeInteger(newIndex+PCB_rsLengthBeginner, task.resource.length);
+        writeShort(newIndex+PCB_tidBeginner, task.tid);
+        writeShort(newIndex+PCB_arrivedTimeBeginner, getTimeTick());
+        writeShort(newIndex+PCB_cpuTimeBeginner, task.cpuTime);
+        writeShort(newIndex+PCB_leftTimeBeginner, task.cpuTime);
+        writeShort(newIndex+PCB_rsLengthBeginner, task.resource.length);
         for(int i = 0 ; i < task.resource.length; i++) {
             writeFreeMemory(newIndex+PCB_resourceBeginner+i, (byte) task.resource[i]);
         }
-        writeInteger(latestTaskBeginner, task.tid);
-        writeInteger(pcbTableBeginner+task.tid*4, newIndex);
+        writeShort(latestTaskBeginner, task.tid);
+        writeShort(pcbTableBeginner+task.tid*2, newIndex);
+        writeFreeMemory(pcbFinishBeginner + task.tid, (byte) 0);
     }
 
-    private int readInteger(int beginIndex){
-        int ans = 0;
-        ans += (readFreeMemory(beginIndex)&0xff)<<24;
-        ans += (readFreeMemory(beginIndex+1)&0xff)<<16;
-        ans += (readFreeMemory(beginIndex+2)&0xff)<<8;
-        ans += (readFreeMemory(beginIndex+3)&0xff);
+    private short readShort(int beginIndex) {
+        short ans = 0;
+        ans += (readFreeMemory(beginIndex) & 0xff) << 8;
+        ans += (readFreeMemory(beginIndex + 1) & 0xff);
         return ans;
     }
 
-    private void writeInteger(int beginIndex, int value){
-        writeFreeMemory(beginIndex+3, (byte) ((value&0x000000ff)));
-        writeFreeMemory(beginIndex+2, (byte) ((value&0x0000ff00)>>8));
-        writeFreeMemory(beginIndex+1, (byte) ((value&0x00ff0000)>>16));
-        writeFreeMemory(beginIndex, (byte) ((value&0xff000000)>>24));
-    }
-
-    /**
-     * 执行主函数 用于debug
-     * 里面的内容可随意修改
-     * 你可以在这里进行对自己的策略进行测试，如果不喜欢这种测试方式，可以直接删除main函数
-     * @param args 没什么乱用的参数
-     * @throws IOException 实际上不会抛出的异常
-     */
-    public static void main(String[] args) throws IOException {
-        // 定义cpu的数量
-        int cpuNumber = 2;
-        // 定义测试文件
-        String filename = "src/testFile/textSample.txt";
-
-        BottomMonitor bottomMonitor = new BottomMonitor(filename,cpuNumber);
-        BottomService bottomService = new BottomService(bottomMonitor);
-        Schedule schedule =  new S161250220();
-        schedule.setBottomService(bottomService);
-
-        //外部调用实现类
-        for(int i = 0 ; i < 500 ; i++){
-            Task[] tasks = bottomMonitor.getTaskArrived();
-            int[] cpuOperate = new int[cpuNumber];
-
-            // 结果返回给cpuOperate
-            schedule.ProcessSchedule(tasks,cpuOperate);
-
-            try {
-                bottomService.runCpu(cpuOperate);
-            } catch (Exception e) {
-                System.out.println("Fail: "+e.getMessage());
-                e.printStackTrace();
-                return;
-            }
-            bottomMonitor.increment();
-        }
-
-        //打印统计结果
-        bottomMonitor.printStatistics();
-        System.out.println();
-
-        //打印任务队列
-        bottomMonitor.printTaskArrayLog();
-        System.out.println();
-
-        //打印cpu日志
-        bottomMonitor.printCpuLog();
-
-
-        if(!bottomMonitor.isAllTaskFinish()){
-            System.out.println(" Fail: At least one task has not been completed! ");
-        }
+    private void writeShort(int beginIndex, int value) {
+        writeFreeMemory(beginIndex+1, (byte) ((value&0x000000ff)));
+        writeFreeMemory(beginIndex, (byte) ((value&0x0000ff00)>>8));
     }
 
 }
